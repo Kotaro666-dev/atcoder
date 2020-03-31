b2d7c8a451d36b5bd23d220645a0c8176dfd0999
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-03-31 11:03:15 +0900

    Submit A and B from ABC068

---
+++ ABC068/B.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   B.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kotaro666 <kotaro0726@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/31 10:30:03 by kotaro666         #+#    #+#             */
+/*   Updated: 2020/03/31 11:02:58 by kotaro666        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;            //
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    int ans = 1;
+    while (ans * 2 <= N)
+    {
+        ans *= 2;
+    }
+
+    cout << ans << endl;
+    return (0);
+}
