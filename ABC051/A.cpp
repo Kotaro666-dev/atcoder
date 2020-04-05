153adf19e6887d6471180fc148d66bfef805c13e
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-05 19:51:03 +0900

    Submit A and B from ABC051

---
+++ ABC051/A.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kotaro666 <kotaro0726@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/05 19:35:15 by kotaro666         #+#    #+#             */
+/*   Updated: 2020/04/05 19:42:18 by kotaro666        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;            //
+
+int main(void)
+{
+    string str;
+    cin >> str;
+
+    str[5] = ' ';
+    str[13] = ' ';
+    cout << str << endl;
+    return (0);
+}
