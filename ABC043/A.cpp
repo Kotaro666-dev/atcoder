c8d5e424c125e8de9ef71bb128a5015176587959
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-12 23:02:41 +0900

    Submit A from ABC043

---
+++ ABC043/A.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/12 22:59:44 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/04/12 23:01:25 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int N;
+
+    cin >> N;
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        sum += i;
+    }
+    cout << sum << endl;
+    return (0);
+}
