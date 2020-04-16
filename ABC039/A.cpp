a377234c67c0ba48ae014f2ec9be6a23b5199f8b
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-16 22:22:44 +0900

    submit A from ABC039

---
+++ ABC039/A.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/16 22:19:09 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/04/16 22:20:53 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    int ans = 2 * (A * B + B * C + C * A);
+    cout << ans << endl;
+    return (0);
+}
