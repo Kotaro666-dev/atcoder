f5d041367f8cd26df089f0256331dbc7fb8aefcc
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-17 19:18:33 +0900

    Submit A from ABC038

---
+++ ABC038/A.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/17 19:15:04 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/04/17 19:16:53 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    string S;
+
+    cin >> S;
+
+    string ans;
+    if (S.at(S.size() - 1) == 'T')
+    {
+        ans = "YES";
+    }
+    else
+    {
+        ans = "NO";
+    }
+    cout << ans << endl;
+    return (0);
+}
