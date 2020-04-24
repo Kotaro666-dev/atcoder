0a7f1350e59a16e7324fab16b428fd07ed52c46a
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-24 21:20:06 +0900

    Submit A from ABC031

---
+++ ABC031/A.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/24 21:16:45 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/04/24 21:18:59 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int A, D;
+    cin >> A >> D;
+
+    int ans = max(((A + 1) * D), (A * (D + 1)));
+    cout << ans << endl;
+
+    return (0);
+}
