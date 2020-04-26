8da9830eb14718286ba2f615824a96889beb08d5
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-04-26 17:31:02 +0900

    Submit A from ABC029

---
+++ ABC029/A.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/26 17:26:22 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/04/26 17:26:43 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    string S;
+    cin >> S;
+
+    cout << S << "s" << endl;
+    return (0);
+}
