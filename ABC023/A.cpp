a1cf444976312b6af0b53391e2271aee12d26754
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-05-02 20:34:34 +0900

    Submit A from ABC023

---
+++ ABC023/A.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/02 20:32:26 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/05/02 20:33:38 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    int ans = N / 10 + N % 10;
+    cout << ans << endl;
+    return (0);
+}
