ed00b48929f614818fb9f40c4136a0bf5fd40f94
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-05-05 12:36:44 +0900

    Submit A from ABC020

---
+++ ABC020/A.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   A.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/05 12:34:32 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/05/05 12:36:04 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int Q;
+    cin >> Q;
+
+    if (Q == 1)
+    {
+        printf("ABC\n");
+    }
+    else
+    {
+        printf("chokudai\n");
+    }
+    return (0);
+}
