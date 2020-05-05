2451083a79bb2e8d57fcaf1acc57c461bccabc33
釜島　光太郎 <kotarokamashima1@Macbook-Air.local>
2020-05-05 12:38:43 +0900

    init

--- ABC165/A.cpp
+++ ABC165/A.cpp
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 11:37:02 by kkamashi          #+#    #+#             */
-/*   Updated: 2020/05/02 20:48:57 by kkamashi         ###   ########.fr       */
+/*   Updated: 2020/05/02 21:13:15 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,29 @@ const double EPS = 1e-10;
 
 int main(void)
 {
+    int K;
+    cin >> K;
 
+    int A, B;
+    cin >> A >> B;
+
+    string ans;
+    int temp;
+    for (int i = 1;; i++)
+    {
+        temp = K * i;
+        if (temp >= A && temp <= B)
+        {
+            ans = "OK";
+            break;
+        }
+        else if (temp > B)
+        {
+            ans = "NG";
+            break;
+        }
+    }
+
+    cout << ans << endl;
     return (0);
 }
--- ABC165/B.cpp
+++ ABC165/B.cpp
@@ -6,12 +6,19 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 11:37:17 by kkamashi          #+#    #+#             */
-/*   Updated: 2020/05/02 20:48:54 by kkamashi         ###   ########.fr       */
+/*   Updated: 2020/05/02 21:53:27 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <bits/stdc++.h>
-#define _GLIBCXX_DEBUG
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+#include <bits/stdc++.h>
 #define PI 3.1415926535897
 using namespace std;
 typedef long long ll;
@@ -21,6 +28,37 @@ const double EPS = 1e-10;
 
 int main(void)
 {
+    ll X;
+    cin >> X;
 
+    int ans;
+    int current = 100;
+    int i = 0;
+    for (;;)
+    {
+        
+        i++;
+    }
     return (0);
 }
+
+// int main(void)
+// {
+//     ll X;
+//     cin >> X;
+
+//     int ans;
+//     int current = 100;
+//     for (int i = 0;; i++)
+//     {
+//         if (current >= X)
+//         {
+//             ans = i;
+//             break;
+//         }
+//         current += (current * 1 / 100);
+//         // printf("%d\n", current);
+//     }
+//     cout << ans << endl;
+//     return (0);
+// }
