5bed0dafad463b2c5ee35018bbc079e541516b39
Kotaro666 <kotaro0726@gmail.com>
2020-05-26 12:27:40 +0900

    Submit B from ABC020

---
+++ ABC020/B.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   B.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/26 11:51:42 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/05/26 12:26:54 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    string A, B;
+    cin >> A >> B;
+
+    ll ans;
+    string word = "";
+    word += A;
+    word += B;
+
+    ans = stoi(word) * 2;
+    cout << ans << endl;
+    return (0);
+}
