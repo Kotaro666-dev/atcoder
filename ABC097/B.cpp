48a70f29e63dd6c2fbf26c197d9e92966eef491c
Kotaro666 <kotaro0726@gmail.com>
2020-06-04 11:03:24 +0900

    Submit B from ABC097

--- ABC097/B.cpp
+++ ABC097/B.cpp
@@ -3,49 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   B.cpp                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kotaro666 <kotaro0726@gmail.com>           +#+  +:+       +#+        */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/22 19:16:06 by kotaro666         #+#    #+#             */
-/*   Updated: 2020/03/22 19:54:34 by kotaro666        ###   ########.fr       */
+/*   Updated: 2020/06/04 11:01:46 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-I cannot make this task AC.....
- */
 
 #include <bits/stdc++.h>
 #define PI 3.1415926535897
 using namespace std;
 typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
 
 int main(void)
 {
-    int X;
-    cin >> X;
+    int x;
+    cin >> x;
 
-    int ans = 1;
-    int result;
-    for (int i = 1; i < X; i++)
+    vector<bool> expo(x + 1);
+    expo[1] = true;
+
+    for (int i = 2; i <= x; i++)
     {
-        for (int j = 2; j <= X; j++)
+        int v = i * i;
+        while (v <= x)
         {
-            result = j;
-            for (int k = 0; k < 100; k++)
-            {
-                result *= j;
-                if (result == i)
-                {
-                    ans = i;
-                    break;
-                }
-                if (result > X)
-                {
-                    break;
-                }
-            }
+            expo[v] = 1;
+            v *= i;
         }
     }
-    cout << ans << endl;
-    return (0);
+
+    for (int i = x; i >= 1; i--)
+    {
+        if (expo[i])
+        {
+            cout << i << endl;
+            return (0);
+        }
+    }
 }
+
+// int main(void)
+// {
+//     int X;
+//     cin >> X;
+
+//     int num = sqrt(X);
+//     int ans = num * num;
+//     cout << ans << endl;
+//     return (0);
+// }
+
+// #include <bits/stdc++.h>
+// #define PI 3.1415926535897
+// using namespace std;
+// typedef long long ll;
+
+// int main(void)
+// {
+//     int X;
+//     cin >> X;
+
+//     int ans = 1;
+//     int result;
+//     for (int i = 1; i < X; i++)
+//     {
+//         for (int j = 2; j <= X; j++)
+//         {
+//             result = j;
+//             for (int k = 0; k < 100; k++)
+//             {
+//                 result *= j;
+//                 if (result == i)
+//                 {
+//                     ans = i;
+//                     break;
+//                 }
+//                 if (result > X)
+//                 {
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     cout << ans << endl;
+//     return (0);
+// }
