b9186f01960ccce8856d4d63bf9e626796f9f5e5
Kotaro666 <kotaro0726@gmail.com>
2020-06-06 14:44:22 +0900

    Submit C from ABC002

---
+++ ABC002/C.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   C.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/06 14:19:59 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/06/06 14:43:08 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    std::cout << std::fixed << std::setprecision(3);
+    double x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+
+    x2 -= x1;
+    x3 -= x1;
+    y2 -= y1;
+    y3 -= y1;
+    x1 = 0;
+    y1 = 0;
+    long double ans;
+    ans = abs(x2 * y3 - y2 * x3) * 0.5;
+    cout << ans << endl;
+    return (0);
+}
