1a8ddfadb44544336ca7f1a31dc67f012195136b
Kotaro666 <kotaro0726@gmail.com>
2020-06-11 11:03:09 +0900

    Submit C from ABC157: Failed because of missing case

--- ABC157/C.cpp
+++ ABC157/C.cpp
@@ -3,144 +3,220 @@
 /*                                                        :::      ::::::::   */
 /*   C.cpp                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kotaro666 <kotaro0726@gmail.com>           +#+  +:+       +#+        */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 18:12:43 by kotaro666         #+#    #+#             */
-/*   Updated: 2020/03/01 23:11:56 by kotaro666        ###   ########.fr       */
+/*   Updated: 2020/06/11 10:58:37 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <bits/stdc++.h>
+#define PI 3.1415926535897
 using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
 
-int main()
+int main(void)
 {
     int N, M;
     cin >> N >> M;
 
-    vector<vector<int>> digit_num(M, vector<int>(2));
-    // vector<int> num(M);
+    if (M == 0)
+    {
+        if (N == 1)
+        {
+            printf("0\n");
+        }
+        else if (N == 2)
+        {
+            printf("10\n");
+        }
+        else if (N == 3)
+        {
+            printf("100\n");
+        }
+        return (0);
+    }
 
+    vector<int> index(M);
+    vector<int> value(M);
     for (int i = 0; i < M; i++)
     {
-        for (int j = 0; j < 2; j++)
+        cin >> index[i] >> value[i];
+
+        // 例外処理
+        if (N >= 2)
         {
-            cin >> digit_num.at(i).at(j);
+            if (index[i] == 1 && value[i] == 0)
+            {
+                printf("-1\n");
+                return (0);
+            }
         }
     }
 
-    // for (int i = 0; i < M; i++)
-    // {
-    //     // for (int j = 0; j < 2; j++)
-    //     // {
-    //     cout << digit_num.at(i).at(0) << " ";
-    //     // }
-    //     cout << endl;
-    // }
-
-    if (M == 0)
-    {
-        cout << "-1" << endl;
-        return 0;
-    }
-
-    int ans = -1;
+    int keta1;
+    int keta2;
+    int keta3;
+    bool found1 = false;
+    bool found2 = false;
+    bool found3 = false;
 
     if (N == 1)
     {
-        bool found = false;
-        ans = 9;
         for (int i = 0; i < M; i++)
         {
-            if (digit_num.at(i).at(0) == 1)
+            if (found1 == false)
             {
-                found = true;
-                ans = min(ans, digit_num.at(i).at(1));
+                keta1 = value[i];
+                found1 = true;
+            }
+            else
+            {
+                if (keta1 == 0 || keta1 != value[i])
+                {
+                    printf("-1\n");
+                    return (0);
+                }
             }
         }
-        if (found == false)
-        {
-            ans = -1;
-        }
+        printf("%d\n", keta1);
     }
     else if (N == 2)
     {
-        int first_digit = 9, second_digit = 9;
-        bool first_found = false;
-        bool second_found = false;
         for (int i = 0; i < M; i++)
         {
-            if (digit_num.at(i).at(0) == 1 && digit_num.at(i).at(1) != 0)
+            if (index[i] == 1)
             {
-                first_found = true;
-                first_digit = min(first_digit, digit_num.at(i).at(1));
+                if (found1 == false)
+                {
+                    keta1 = value[i];
+                }
+                else
+                {
+                    if (keta1 != value[i])
+                    {
+                        printf("-1\n");
+                        return (0);
+                    }
+                }
+                found1 = true;
             }
-            if (digit_num.at(i).at(0) == 2)
+            else if (index[i] == 2)
             {
-                second_found = true;
-                second_digit = min(second_digit, digit_num.at(i).at(1));
+                if (found2 == false)
+                {
+                    keta2 = value[i];
+                }
+                else
+                {
+                    if (keta2 != value[i])
+                    {
+                        printf("-1\n");
+                        return (0);
+                    }
+                }
+                found2 = true;
             }
         }
-        if (first_digit == false && second_digit == false)
-            ans = -1;
-        else if (first_digit == false && second_digit == true)
-            ans = 10 + second_digit;
-        else if (first_digit == true && second_digit == false)
-            ans = 10 * first_digit;
-        else if (first_digit == true && second_digit == true)
-            ans = first_digit * 10 + second_digit;
+
+        if (found1 == false && found2 == true)
+        {
+            printf("%d\n", 10 + keta2);
+        }
+        else if (found1 == true && found2 == false)
+        {
+            printf("%d\n", keta1 * 10);
+        }
+        else if (found1 == true && found2 == true)
+        {
+            printf("%d\n", keta1 * 10 + keta2);
+        }
     }
     else if (N == 3)
     {
-        int first_digit = 9, second_digit = 9, third_digit = 9;
-        bool first_found = false;
-        bool second_found = false;
-        bool third_found = false;
         for (int i = 0; i < M; i++)
         {
-            if (digit_num.at(i).at(0) == 1 && digit_num.at(i).at(1) != 0)
+            if (index[i] == 1)
             {
-                first_found = true;
-                first_digit = min(first_digit, digit_num.at(i).at(1));
+                if (found1 == false)
+                {
+                    keta1 = value[i];
+                    found1 = true;
+                }
+                else
+                {
+                    if (keta1 != value[i])
+                    {
+                        printf("-1\n");
+                        return (0);
+                    }
+                }
             }
-            if (digit_num.at(i).at(0) == 2 && digit_num.at(i).at(1) != 0)
+            else if (index[i] == 2)
             {
-                second_found = true;
-                second_digit = min(second_digit, digit_num.at(i).at(1));
+                if (found2 == false)
+                {
+                    keta2 = value[i];
+                    found2 = true;
+                }
+                else
+                {
+                    if (keta2 != value[i])
+                    {
+                        printf("-1\n");
+                        return (0);
+                    }
+                }
             }
-            if (digit_num.at(i).at(0) == 3)
+            else if (index[i] == 3)
             {
-                third_found = true;
-                third_digit = min(third_digit, digit_num.at(i).at(1));
+                if (found3 == false)
+                {
+                    keta3 = value[i];
+                    found3 = true;
+                }
+                else
+                {
+                    if (keta3 != value[i])
+                    {
+                        printf("-1\n");
+                        return (0);
+                    }
+                }
             }
         }
-        // cout << first_digit << endl;
-        // cout << ans << endl;
 
-        if (first_digit == 0)
-            ans = -1;
-        if (first_digit == 0 && second_digit == 0)
-            ans = -1;
-        if (first_found == true && second_found == true && third_found == true)
-            ans = first_digit * 100 + second_digit * 10 + third_digit;
-        else if (first_found == true && second_found == true && third_found == false)
-            ans = first_digit * 100 + second_digit * 10;
-        else if (first_found == true && second_found == false && third_found == false)
-            ans = first_digit * 100;
-        else if (first_found == false && second_found == false && third_found == false)
-            ans = -1;
-        else if (first_found == false && second_found == true && third_found == false)
-            ans = -1;
-        else if (first_found == false && second_found == false && third_found == true)
-            ans = 100 + third_digit;
-        else if (first_found == false && second_found == true && third_found == true)
-            ans = 100 + second_digit * 10 + first_digit;
-        else if (first_found == true && second_found == false && third_found == false)
-            ans = first_digit * 100;
-        else if (first_found == true && second_found == false && third_found == true)
-            ans = first_digit * 100 + third_digit;
-        else
-            ans = -1;
+        if (found1 == true && found2 == true && found3 == true)
+        {
+            printf("%d\n", keta1 * 100 + keta2 * 10 + keta3);
+        }
+        else if (found1 == true && found2 == true && found3 == false)
+        {
+            printf("%d\n", keta1 * 100 + keta2 * 10);
+        }
+        else if (found1 == true && found2 == false && found3 == true)
+        {
+            printf("%d\n", keta1 * 100 + keta3);
+        }
+        else if (found1 == true && found2 == false && found3 == false)
+        {
+            printf("%d\n", keta1 * 100);
+        }
+        else if (found1 == false && found2 == true && found3 == false)
+        {
+            printf("%d\n", 100 + keta2 * 10);
+        }
+        else if (found1 == false && found2 == true && found3 == true)
+        {
+            printf("%d\n", 100 + keta2 * 10 + keta3);
+        }
+        else if (found1 == false && found2 == false && found3 == true)
+        {
+            printf("%d\n", 100 + keta3);
+        }
     }
-    cout << ans << endl;
+    return (0);
 }
