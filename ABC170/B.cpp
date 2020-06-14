aee237077cd70442863888bd0c01c9b79d0ee1b0
Kotaro666 <kotaro0726@gmail.com>
2020-06-14 14:57:28 +0900

    init

---
+++ ABC170/B.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   B.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/14 14:57:13 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/06/14 14:57:15 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+
+    return (0);
+}
