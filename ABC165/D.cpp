2043f38e7579cdcf109dfbc2023e2778e0394cac
Kotaro666 <kotaro0726@gmail.com>
2020-06-18 10:08:55 +0900

    Submit D from ABC165: Almost AC

--- ABC165/D.cpp
+++ ABC165/D.cpp
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 11:37:28 by kkamashi          #+#    #+#             */
-/*   Updated: 2020/05/02 11:37:30 by kkamashi         ###   ########.fr       */
+/*   Updated: 2020/06/18 10:06:56 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@ const double EPS = 1e-10;
 
 int main(void)
 {
+    ll A, B, N;
+    cin >> A >> B >> N;
 
+    ll X = min(N, B - 1);
+    ll ans = floor((A * X) / B) - A * floor(X / B);
+    // ll ans2 = floor((A * N) / B) - A * floor(N / B);
+
+    // for (int x = 1; x <= N; x++)
+    // {
+    //     ans = floor((A * x) / B) - A * floor(x / B);
+    //     printf("x = %d, ans = %d\n", x, ans);
+    // }
+    cout << ans << endl;
     return (0);
 }
