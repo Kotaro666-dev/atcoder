3a9162531b85bc88c3b73a90e367f3f4288c6f04
Kotaro666 <kotaro0726@gmail.com>
2020-06-18 11:52:02 +0900

    Submit D from ABC143: Failed: 二分探索

---
+++ ABC143/D.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   D.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/18 11:24:08 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/06/18 11:51:34 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// https://drken1215.hatenablog.com/entry/2019/10/20/032700
+// 3つのうち、2つを固定して考える
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000; //1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+
+    vector<int> L(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> L[i];
+    }
+    sort(L.begin(), L.end());
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", L[i]);
+    }
+
+    ll ans = 0;
+    // a と bを固定 -> c < a + b
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            // c の動ける範囲の右端を求める
+            int k = lower_bound(L.begin(), L.end(), L[i] + L[j]) - L.begin();
+
+            // c の動ける範囲は、([j + 1], k)
+            ans += max(k - (j + 1), 0);
+        }
+    }
+    cout << ans << endl;
+    return (0);
+}
