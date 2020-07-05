d19b9cb02239ce38f46d5b83f8c5fd891252b4ee
Kotaro666 <kotaro0726@gmail.com>
2020-07-05 13:10:40 +0900

    init

---
+++ ABC173/D.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   D.cpp                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/05 13:10:10 by kkamashi          #+#    #+#             */
+/*   Updated: 2020/07/05 13:10:11 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <bits/stdc++.h>
+#define PI 3.1415926535897
+using namespace std;
+typedef long long ll;
+const int INF = 1000000000;
+const ll LINF = 1000000000000000000;//1e18
+const double EPS = 1e-10;
+
+int main(void)
+{
+
+    return (0);
+}
